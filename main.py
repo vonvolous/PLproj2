1f594ed71db7b7335642a6c9c952a1135ad0a974
Yoo Han <hanyoojin39@gmail.com>
2022-12-22 12:18:24 +0900

    예제 txtfile 실행결과 구현 (syntax 분석 및 함수 실행)

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ next_token = 0  # token type 저장
 
 # error declarations
 syntax_error = False
-syntax_error_msg = "Syntax Error."
-syntax_ok_msg = "Syntax O.K."
+syntax_error_msg = "Syntax Error.\n"
+syntax_ok_msg = "Syntax O.K.\n"
 
 # for getting inputs from txt file
 f = open("sample.txt", 'r')  # txt파일 이름을 argv[1]로 인자로 받기 open(sys.argv[1], 'r')로 바꿔주기!!
@@ -39,7 +39,7 @@ input += "$"
 
 # functions for grammar
 def start(): # <start> -> <functions>
-    print("start checking grammar")
+    #print("start checking grammar")
     getChar()
     lexical()
     functions()
@@ -149,22 +149,22 @@ def lookup(ch):
     if ch == '{':
         addChar()
         next_token = left_paren
-        print(token_string)
+        #print(token_string)
         tokenized_text.append([token_string, next_token])
     elif ch == '}':
         addChar()
         next_token = right_paren
-        print(token_string)
+        #print(token_string)
         tokenized_text.append([token_string, next_token])
     elif ch == ',':
         addChar()
         next_token = comma
-        print(token_string)
+        #print(token_string)
         tokenized_text.append([token_string, next_token])
     elif ch == ';':
         addChar()
         next_token = semi_colon
-        print(token_string)
+        #print(token_string)
         tokenized_text.append([token_string, next_token])
     elif ch == '$':
         addChar()
@@ -209,19 +209,19 @@ def lexical():
             getChar()
         if token_string == "call":
             next_token = reserved_word
-            print(token_string)
+            #print(token_string)
             tokenized_text.append([token_string,next_token])
         elif token_string == "print_ari":
             next_token = reserved_word
-            print(token_string)
+            #print(token_string)
             tokenized_text.append([token_string,next_token])
         elif token_string == "variable":
             next_token = reserved_word
-            print(token_string)
+            #print(token_string)
             tokenized_text.append([token_string,next_token])
         else:
             next_token = ident
-            print(token_string)
+            #print(token_string)
             tokenized_text.append([token_string,next_token])
     elif charClass == UNKNOWN:
         lookup(nextChar)
@@ -294,6 +294,7 @@ if not syntax_error:
 
     # main함수 있는 경우 프로그램 실행!
     if main:
+        caller_func=''
         ari.append(['main', activation_record_instance['main']])
         i = function_idx[function_name.index('main')]
         i += 1 # '{'
@@ -305,6 +306,37 @@ if not syntax_error:
                 function_line -= 1
             elif tokenized_text[i][0] == ';':
                 function_line += 1
+            elif tokenized_text[i][0] == "print_ari":  # print_ari 나오면 ari 출력해주기
+                print("hi ari~", ari)
+            elif (tokenized_text[i - 1][0] == '{' and tokenized_text[i][1] == 11) or (
+                    tokenized_text[i - 1][0] == ';' and tokenized_text[i][1] == 11):
+                link_count = 0
+                local_offset = 0
+                found = False
+                sample_idx = function_name.index('main') + 1
+
+                while 1:
+                    if sample_idx < 0:
+                        break
+                    while 1:
+                        if ari[sample_idx][1][local_offset] == tokenized_text[i][0]:
+                            found = True
+                            break
+                        else:
+                            local_offset += 1
+                            if local_offset >= len(ari[sample_idx][1]):
+                                local_offset = 0
+                                break
+                    if found:
+                        sample_string = "main" + ":" + str(tokenized_text[i][0]) + " => " + str(
+                            link_count) + ", " + str(local_offset) +"\n"
+                        print(sample_string)
+                        link_count = 0
+                        local_offset = 0
+                        break
+                    link_count += 1
+                    sample_idx -= 1
+                    local_offset = 0
             elif tokenized_text[i][0] == 'call':
                 func_list = activation_record_instance[tokenized_text[i+1][0]]
                 func_list[0] = ['main', function_line + 1]
@@ -312,41 +344,154 @@ if not syntax_error:
                 ep += 2
                 ari.append([tokenized_text[i+1][0], func_list])
                 func = tokenized_text[i+1][0]
+                caller_func = func
+                dynamic_link.append(func)
 
                 # 메인에서 호출한 first 함수로 넘어감
                 j = function_idx[function_name.index(func)]
                 j += 1 # '{'
-                caller_func = func
                 while 1:
                     j += 1
-                    function_line = 0
-                    if tokenized_text[j][0] == '}':  # sub function 끝나면 종료
+                    function_line1 = 0
+                    if tokenized_text[j][0] == '}':  # first sub function 끝나면 종료
+                        ari.pop()
+                        #print("after finishing first and pop first ", ari)
                         break
                     elif tokenized_text[j][0] == 'variable':
-                        function_line -= 1
+                        function_line1 -= 1
                     elif tokenized_text[j][0] == ';':
-                        function_line += 1
+                        function_line1 += 1
+                    elif tokenized_text[j][0] == "print_ari":  # print_ari 나오면 ari 출력해주기
+                        print("hi ari~", ari)
+                    elif (tokenized_text[j - 1][0] == '{' and tokenized_text[j][1] == 11) or (
+                            tokenized_text[j - 1][0] == ';' and tokenized_text[j][1] == 11):
+                        link_count = 0
+                        local_offset = 0
+                        found = False
+                        sample_idx = function_name.index(caller_func) + 1
+
+                        while 1:
+                            if sample_idx < 0:
+                                break
+                            while 1:
+                                if ari[sample_idx][1][local_offset] == tokenized_text[j][0]:
+                                    found = True
+                                    break
+                                else:
+                                    local_offset += 1
+                                    if local_offset >= len(ari[sample_idx][1]):
+                                        local_offset = 0
+                                        break
+                            if found:
+                                sample_string = caller_func+":"+str(tokenized_text[j][0])+" => "+str(link_count)+", "+str(local_offset) +"\n"
+                                print(sample_string)
+                                link_count = 0
+                                local_offset = 0
+                                break
+                            link_count += 1
+                            sample_idx -= 1
+                            local_offset = 0
                     elif tokenized_text[j][0] == 'call':
                         func_list = activation_record_instance[tokenized_text[j + 1][0]]
-                        func_list[0] = [caller_func, function_line + 1]
+                        func_list[0] = [caller_func, function_line1 + 1]
                         func_list[1] = ep
                         ep += len(func_list)
                         ari.append([tokenized_text[j + 1][0], func_list])
                         func = tokenized_text[j + 1][0]
+                        dynamic_link.append(func)
+
+                        # first에서 호출한 second 함수로 넘어감
+                        k = function_idx[function_name.index(func)]
+                        k += 1 # '{'
+                        caller_func = func
+                        while 1:
+                            k += 1
+                            function_line2 = 0
+                            if tokenized_text[k][0] == '}': # second sub function 끝나면 종료
+                                dynamic_link.pop()
+                                caller_func = dynamic_link.pop()
+                                ari.pop()
+                                #print("after finishing second and pop second ", ari)
+                                break
+                            elif tokenized_text[k][0] == "print_ari":  # print_ari 나오면 ari 출력해주기
+                                #print("hi ari!~", ari)
+                                for ari_idx in range(len(ari)-1,-1,-1):
+                                    for stack_idx in range(len(ari[ari_idx][1])-1,-1,-1):
+                                        if ari[ari_idx][0] == 'main':
+                                            if stack_idx == len(ari[ari_idx][1])-1:
+                                                local_string = str(ari[ari_idx][0])+":Local variable: "+str(ari[ari_idx][1][stack_idx])
+                                                print(local_string)
+                                            else:
+                                                local_string = "Local variable: "+str(ari[ari_idx][1][stack_idx])
+                                                print(local_string)
+                                        else:
+                                            if stack_idx == len(ari[ari_idx][1])-1:
+                                                local_string = str(ari[ari_idx][0])+":Local variable: "+str(ari[ari_idx][1][stack_idx])
+                                                print(local_string)
+                                            elif stack_idx == 0: # ret
+                                                ret_string = "Return Address: "+str(ari[ari_idx][1][stack_idx][0])+":"+str(ari[ari_idx][1][stack_idx][1])+"\n"
+                                                print(ret_string)
+                                            elif stack_idx == 1: # dynamic link
+                                                dynamic_string = "Dynamic Link: "+str(ari[ari_idx][1][stack_idx])
+                                                print(dynamic_string)
+                                            else:
+                                                local_string = "Local variable: "+str(ari[ari_idx][1][stack_idx])
+                                                print(local_string)
+
+                                    if ari[ari_idx][0] == 'main':
+                                        print("\n")
+
+
+
+                            elif (tokenized_text[k-1][0] == '{' and tokenized_text[k][1] == 11) or (tokenized_text[k-1][0] == ';' and tokenized_text[k][1] == 11):
+                                link_count = 0
+                                local_offset = 0
+                                found = False
+                                sample_idx = function_name.index(caller_func)+1
+
+                                while 1:
+                                    if sample_idx < 0:
+                                        break
+                                    while 1:
+                                        if ari[sample_idx][1][local_offset] == tokenized_text[k][0]:
+                                            found = True
+                                            break
+                                        else:
+                                            local_offset += 1
+                                            if local_offset >= len(ari[sample_idx][1]):
+                                                local_offset = 0
+                                                break
+                                    if found:
+                                        sample_string = caller_func + ":" + str(tokenized_text[k][0]) + " => " + str(
+                                            link_count) + ", " + str(local_offset)+"\n"
+                                        print(sample_string)
+                                        link_count = 0
+                                        local_offset = 0
+                                        break
+                                    link_count += 1
+                                    sample_idx -= 1
+                                    local_offset = 0
+
+                                func_list = [0,0]
+                                k += 1
+
+                        func_list = [0,0]
+                        j += 1
                 func_list = [0,0]
-                i += 2
+                i += 1
 
 
-    print("ari ", ari)
+    #print("ari ", ari)
+
     # ari에 dynamic link 넣어주기, ep 넣어주기...호출한 함수의 가장 밑의 주소
     #for
 
     # ari에 return address 넣어주기, 함수명: 실행문 위치
 
-print(tokenized_text)
-print(activation_record_instance)
-print(function_idx)
-print(function_name)
+#print(tokenized_text)
+#print(activation_record_instance)
+#print(function_idx)
+#print(function_name)
 
 '''
 for i in tokenized_text:
